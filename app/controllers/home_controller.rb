24a1d2d177feacfd1ec623acea55a92b7f42e59d
Bodhish Thomas <bodhish@gmail.com>
2019-06-01 23:02:32 +0530

    Save params and visits

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,29 @@
 class HomeController < ApplicationController
   def index
-    @name = params[:name]
 
-    @count = Visit.where(title: "index").first
+    @name = params[:name]&.parameterize&.split('-')&.join(' ')&.humanize
+
     if @name.present?
-      @count.update!(number: @count.number + 1)
+      @visits = Visit.where(title: @name)
+      if @visits.any?
+        @visits.first.update(number: @visits.first.number + 1)
+      else
+        @visits = [create_name]
+      end
+      @visit = @visits.first
     end
+    add_total_count
+  end
+
+
+  private
+
+  def add_total_count
+    @count = Visit.where(title: "index").first
+    @count.update!(number: @count.number + 1)
+  end
+
+  def create_name
+    Visit.create!(title: @name, number: 1)
   end
 end
